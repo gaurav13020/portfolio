182bc1b5a90297e78c6a20996ced1369286ca368
Gaurav Singh <75926877+gaurav13020@users.noreply.github.com>
2023-12-03 17:23:08 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,13 +40,13 @@ export default function Home() {
               /> */}
               <AnimatedText
                 text="Hi!👋 I'm Gaurav"
-                className="text-7xl  !text-left xl:!text-5xl lg:!text-center lg:!text-5xl lg:!mb-1  md:!text-5xl sm:!text-xl sm:!mb-1  ml-10 pl-10"
+                className="text-7xl  !text-left xl:!text-5xl lg:!text-center lg:!text-5xl lg:!mb-1  md:!text-5xl sm:!text-xl sm:!mb-1  xl:ml-10 xl:pl-10"
                 fontColour="!text-black"
               />
               <AnimatedText
                 text="Machine Learning | Data Science Engineer"
                 fontColour="!text-black-500"
-                className="text-3xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
+                className="text-3xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl xl:ml-10 xl:pl-10"
               />
               <ul className="my-4 text-base font-medium md:text-sm sm:text-xs ">
                 <li className="m-1">I&apos;m a Machine Learning and Data Science Engineer passionate about 
